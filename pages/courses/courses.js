7f6636a6e8ebeba8b2522b39ddd893ef97e226af
Molly Clawson <mmclawson@icloud.com>
2021-12-12 13:09:01 -0500

    courses.js

--- pages/courses/courses.js
+++ pages/courses/courses.js
@@ -1,21 +1,23 @@
 var http = require('http');
 var qs = require('querystring');
+var express = require('express');
+const path = require('path');
+const app = express();
 const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb+srv://mollyclaw:Whiplash2015@cluster0.yqtdj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"; 
+const url = "mongodb+srv://mollyclaw:Whiplash2015@cluster0.yqtdj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+app.use(express.static(path.join(__dirname, `public`)));
 
 http.createServer(function (req, res) 
 {
     res.writeHead(200, {'Content-Type':'text/html'});
-    res.write("hello world");
 
     if (req.url == "/") {
         console.log("1");
         res.write("in the default");
 
     } else if (req.url == "/process") {
-        console.log("2");
-        
-        res.write("in the process");
+        res.write("<div> Your schedule has been saved! </div>");
         pdata = "";
 		req.on('data', data => {
             pdata += data.toString();
@@ -39,13 +41,9 @@ http.createServer(function (req, res)
                 var newData = {"User": "molly.clawson@tufts.edu", "Schedule": userCourses}
               
                 coll.insertOne(newData);
-                console.log(newData);
-              
+                console.log(newData);    
               });
-
 		});
-
-
     }
     res.end();
 
