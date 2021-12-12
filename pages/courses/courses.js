7c674f0f6f48ad41b4b6e3ad53fdd94efe4ddd9c
Molly Clawson <mmclawson@icloud.com>
2021-12-12 11:50:11 -0500

    courses.html almost complete

---
+++ pages/courses/courses.js
@@ -0,0 +1,53 @@
+var http = require('http');
+var qs = require('querystring');
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb+srv://mollyclaw:Whiplash2015@cluster0.yqtdj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"; 
+
+http.createServer(function (req, res) 
+{
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.write("hello world");
+
+    if (req.url == "/") {
+        console.log("1");
+        res.write("in the default");
+
+    } else if (req.url == "/process") {
+        console.log("2");
+        
+        res.write("in the process");
+        pdata = "";
+		req.on('data', data => {
+            pdata += data.toString();
+        });
+
+        var userCourses = [];
+
+        req.on('end', () => {
+			pdata = qs.parse(pdata);
+            for (i = 0; i < 8; i++) {
+                courses = pdata['courses'+ i]
+                userCourses[i] = courses;
+            }
+            console.log(userCourses);
+
+            MongoClient.connect(url, function(err, db) {
+                if (err) throw err;
+                var dbo = db.db("breaking-grad");
+                var coll = dbo.collection("user-data");
+    
+                var newData = {"User": "molly.clawson@tufts.edu", "Schedule": userCourses}
+              
+                coll.insertOne(newData);
+                console.log(newData);
+              
+              });
+
+		});
+
+
+    }
+    res.end();
+
+}).listen(8080);
+
