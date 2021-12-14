20b4f6287b5dc94ffb4a26ac4a88ee0c5b5bfd83
CWKRowan <87955550+CWKRowan@users.noreply.github.com>
2021-12-14 18:00:37 -0500

    Update courses.js

--- pages/courses/courses.js
+++ pages/courses/courses.js
@@ -10,6 +10,9 @@ app.use(express.static(path.join(__dirname, `public`)));
 
 http.createServer(function (req, res)
 {
+
+  res.writeHead(405, headers);
+  res.end(`${req.method} is not allowed for the request.`);
     res.writeHead(200, {'Content-Type':'text/html'});
 
     if (req.url == "/") {
@@ -79,6 +82,24 @@ http.createServer(function (req, res)
               });
 		});
     }
+	const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+    'Access-Control-Max-Age': 2592000, // 30 days
+    /** add other headers as per requirement */
+	  };
+
+	  if (req.method === 'OPTIONS') {
+	    res.writeHead(204, headers);
+	    res.end();
+	    return;
+	  }
+
+	  if (['GET', 'POST'].indexOf(req.method) > -1) {
+	    res.writeHead(200, headers);
+	    res.end('Hello World');
+	    return;
+	  }
     res.end();
 
 }).listen(process.env.PORT);
